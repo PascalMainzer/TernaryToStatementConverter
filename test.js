0af494599037454bfc0a9bac1c92548aacd7936a
PascalMainzer <fabianpascalm@gmail.com>
2022-08-03 23:18:43 +0100

    showTestBlue1

--- test.js
+++ test.js
@@ -27,56 +27,119 @@ function showTest(testFunction, testString) {
 
 
 function singleTernaryTest(string) {
-    var findStart = function(stringBefore, wordLength) {
-        var indexOfVar = string.indexOf(stringBefore);
-        var stringStart;
-        for (let i = indexOfVar + wordLength; i < string.length; i++) {
-            if (string[i] != " ") {
-                stringStart = i;
-                break;
-            }
-        }
-        return stringStart;
-    }
-
     var lookUpVariableTerminator = " ?:="
 
-    var findEnd = function(stringStart) {
-        var stringEnd;
-        for (let i = stringStart; i < string.length; i++) {
-            for (let e = 0; e < lookUpVariableTerminator.length; e++) {
-                if (string[i] == lookUpVariableTerminator[e]) {
-                    stringEnd = i;
-                }
-            }
-            if (stringEnd != null) {
-                break;
+    function isTerminator(charToCheck) {
+        for (let terminator = 0; terminator < lookUpVariableTerminator.length; terminator++) {
+            if (charToCheck == lookUpVariableTerminator[terminator]) {
+                return true;
             }
         }
-        return stringEnd;
     }
-    var variableStart = findStart("var ", 3);
-    var variableEnd = findEnd(variableStart);
-    var variableName = string.substring(variableStart, variableEnd);
+    var findStart = function(string, wordBeforeVariable, wordLength) {
+        var indexOfVar;
+        if (wordBeforeVariable == "") {
+            indexOfVar = 0;
+        }
+        indexOfVar = string.indexOf(wordBeforeVariable);
+        if (indexOfVar == -1) {
+            return -1;
+        }
 
-    var conditionStart = findStart("=", 1);
-    var conditionEnd = findEnd(conditionStart);
-    var condition = string.substring(conditionStart, conditionEnd);
+        for (let i = indexOfVar + wordLength; i < string.length; i++) {
+            if (string[i] != " ") {
+                return i;
+            }
+        }
+    }
 
-    var trueStart = findStart("?", 1);
-    var trueEnd = findEnd(trueStart);
-    var trueValue = string.substring(trueStart, trueEnd);
+    var findEnd = function(string, stringStart) {
+        for (let charPosition = stringStart; charPosition < string.length; charPosition++) {
+            if (isTerminator(string[charPosition])) {
+                return charPosition;
+            }
+        }
+    }
 
-    var falseStart = findStart(":", 1);
-    var falseEnd = findEnd(falseStart)
-    var falseValue = string.substring(falseStart, falseEnd);
+    var getVariable = function(string, variableType) {
+        if (variableType != "") {
+            var variableStart = findStart(string, variableType + " ", 3);
+            var variableEnd = findEnd(string, variableStart);
+            return string.substring(variableStart, variableEnd);
+        }
 
+        return "";
+    }
 
-    return "var " + variableName + ";\n\n" +
-        "if(" + condition + "){\n\t" +
-        variableName + " = " + trueValue +
-        ";\n} else {\n\t" +
-        variableName + " = " + falseValue + ";\n}";
+    var getCondition = function(string) {
+        var conditionStart;
+
+        conditionStart = findStart(string, "=", 1);
+
+        if (conditionStart == -1) {
+            conditionStart = findStart(string, "", 1);
+        }
+        var conditionEnd = findEnd(string, conditionStart);
+        return string.substring(conditionStart, conditionEnd);
+    }
+
+    var getTrue = function(string) {
+        var trueStart = findStart(string, "?", 1);
+        var trueEnd = findEnd(string, trueStart);
+        return string.substring(trueStart, trueEnd);
+    }
+
+    var getFalse = function(string) {
+        var falseStart = findStart(string, ":", 1);
+        var falseEnd = findEnd(string, falseStart)
+        return string.substring(falseStart, falseEnd);
+    }
+
+    var getVariableType = function(string) {
+        var variableStart = findStart(string, "var ", 3);
+        if (variableStart != -1) {
+            return "var"
+        }
+        variableStart = findStart(string, "let ", 3);
+        if (variableStart != -1) {
+            return "let"
+        }
+        return "";
+    }
+
+    var result = "";
+
+    var variableType = getVariableType(string);
+
+    var variableF = getVariable(string, variableType);
+
+    if (variableF != "") {
+        result += variableType + " ";
+        result += variableF;
+        result += ";\n\n"
+    }
+
+    var conditionF = getCondition(string);
+    result += "if( "
+    result += conditionF;
+    result += " )"
+
+    var trueF = getTrue(string);
+    result += "{\n\t";
+    if (variableF != "") {
+        result += variableF + " = ";
+    }
+    result += trueF;
+    result += "\n}";
+
+    var falseF = getFalse(string);
+    result += "else{\n\t";
+    if (variableF != "") {
+        result += variableF + " = ";
+    }
+    result += falseF;
+    result += "\n}"
+    return result;
 }
 
 
@@ -88,4 +151,8 @@ function onLoad() {
     showTest(singleTernaryTest, " var z = c?d:e");
     showTest(singleTernaryTest, " var    z=  c  ? d: e");
     showTest(singleTernaryTest, " var    z=  c  ? 10: 20");
+    showTest(singleTernaryTest, " c  ? 10: 20");
+    showTest(singleTernaryTest, " alpha()  ? beta(): gamma()");
+    showTest(singleTernaryTest, " let z = c?d:e");
+    showTest(singleTernaryTest, " let z = (a&&b)?(a&&b):(a&&b)");
 }
